3308276b1ddcb81402665503b8a88662709b539c
King'ori Maina <j@kingori.co>
2015-02-06 01:16:56 +0200

    Add username attribute to user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,12 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
+    before_action :configure_permitted_parameters
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:account_update) << :username
+      devise_parameter_sanitizer.for(:sign_up) << :username
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Validations
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
 end
