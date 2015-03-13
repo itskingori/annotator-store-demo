b416ddb411c1ec1423294a4bbe53d5a27b90d7ff
King'ori Maina <j@kingori.co>
2015-03-13 17:14:31 +0200

    Updates stats API with upvotes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,22 +3,38 @@ class PagesController < ApplicationController
   end
 
   def stats
+    # Get the text and image annotations on the page that matches the set
+    # criteria i.e. the URL
     format_annotorious_input_to_rails_convention_for_search
-    text_annotations = AnnotatorStore::Annotation.where(uri: search_params[:page_url])
-    image_annotations = AnnotatorStore::ImageAnnotation.where(page_url: search_params[:page_url])
+    # text_annotations = AnnotatorStore::Annotation.where(uri: search_params[:page_url])
+    # image_annotations = AnnotatorStore::ImageAnnotation.where(page_url: search_params[:page_url])
+    text_annotations = AnnotatorStore::Annotation.all
+    image_annotations = AnnotatorStore::ImageAnnotation.all
     text_stats = {}
     text_annotations.each { |ta|
-      text_stats[ta[:user]] = 0 if text_stats[ta[:user]].blank?
-      text_stats[ta[:user]] += 1
+      if text_stats[ta[:user]].blank?
+        text_stats[ta[:user]] = {}
+        text_stats[ta[:user]][:count]   = 0
+        text_stats[ta[:user]][:upvotes] = 0
+      end
+      text_stats[ta[:user]][:count]   += 1
+      text_stats[ta[:user]][:upvotes] += ta.upvotes.size
     }
     image_stats = {}
     image_annotations.each { |ia|
-      image_stats[ia[:user]] = 0 if image_stats[ia[:user]].blank?
-      image_stats[ia[:user]] += 1
+      if image_stats[ia[:user]].blank?
+        image_stats[ia[:user]] = {}
+        image_stats[ia[:user]][:count]   = 0
+        image_stats[ia[:user]][:upvotes] = 0
+      end
+      image_stats[ia[:user]][:count]   += 1
+      image_stats[ia[:user]][:upvotes] += ia.upvotes.size
     }
     combined_stats = []
-    text_stats.merge(image_stats){|key, oldval, newval| newval + oldval}.each { |key, value|
-      combined_stats << { username: key, count: value }
+    text_stats.merge(image_stats) { |key, oldval, newval|
+      oldval.merge(newval)  { |key, ov, nv| nv + ov }
+    }.each { |key, value|
+      combined_stats << { username: key, count: value[:count], upvotes: value[:upvotes] }
     }
     render json: combined_stats
   end
