fa36813f65c3c77cdc381d57435d6b33fc9769f6
King'ori Maina <j@kingori.co>
2015-03-23 13:20:07 +0200

    Add confirmable to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Strip leading and trailing whitespace and set to nil if blank
   normalize_attributes :username
---
+++ db/migrate/20150323111049_add_confirmable_to_users.rb
@@ -0,0 +1,9 @@
+class AddConfirmableToUsers < ActiveRecord::Migration
+  def change
+    # Confirmable
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
+  end
+end
