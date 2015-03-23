d28a2d095d5e0c784ce2d6a8aaae159bf3b54e46
King'ori Maina <j@kingori.co>
2015-03-23 13:05:49 +0200

    Redirect to apex domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < ActionController::Base
+  # Callbacks
+  before_action :redirect_to_apex_domain if Rails.env.production?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  # Redirects to the apex domain depending on the criteria
+  #
+  # Check the domain maps all unmatched to blannotator.kingori.co. Since this method is
+  # only run on production - this should be the only domain that shows the app.
+  def redirect_to_apex_domain
+    domain_to_redirect_to = 'blannotator.kingori.co'
+    domain_exceptions = [domain_to_redirect_to]
+    should_redirect = !(domain_exceptions.include? request.host)
+    domain_protocol = request.protocol
+    new_url = "#{domain_protocol}#{domain_to_redirect_to}#{request.fullpath}"
+    redirect_to new_url, status: :moved_permanently if should_redirect
+  end
 end
