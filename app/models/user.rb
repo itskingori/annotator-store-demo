a0f51503926121e494c3ff3ba544b43173d63f29
King'ori Maina <j@kingori.co>
2015-03-23 14:23:14 +0200

    Toggle gamification status of the users as they register

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,23 @@ class User < ActiveRecord::Base
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        format: { with: /\A[\w-]{3,16}\z/, message: "only allows users comprised of 3 to 16 characters of letters and numbers" }
+
+  # Callbacks
+  before_create :set_gamification_mode
+
+  private
+
+  def set_gamification_mode
+    # Get the last user that's confirmed
+    latest_user = User.where.not(confirmed_at: nil).order(created_at: :desc).take
+    # Determine/toggle the gamification status based on the last user ... and
+    # keep gamification = false as the default.
+    if latest_user
+      mode = !latest_user.gamified
+    else
+      mode = false
+    end
+    self.gamified = mode
+    true
+  end
 end
